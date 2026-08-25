80357e84a274cbf9e6d0dca0a61d721c576ca914
Rezendee11 <pedrorsantanna@gmail.com>
2026-08-25 11:37:42 -0300

    Commit 4: Exercício 3 completo!

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nome do estudante: ");
-        String nome = scanner.nextLine();
 
-        Estudante estudante = new Estudante(nome);
-        estudante.insereNotas();
+        System.out.print("Quantos estudantes deseja cadastrar? ");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Média: " + estudante.calculaMedia());
-        System.out.println("Menor nota: " + estudante.menorNota());
+        Estudante[] estudantes = new Estudante[quantidade];
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("Nome do estudante " + (i + 1) + ": ");
+            String nome = scanner.nextLine();
+            Estudante estudante = new Estudante(nome);
+            estudante.insereNotas();
+            estudantes[i] = estudante;
+        }
+
+        Estudante[] aprovados = Aprovador.filtraAprovados(estudantes);
+
+        if (aprovados == null) {
+            System.out.println("Nenhum estudante foi aprovado.");
+        } else {
+            System.out.println("Estudantes aprovados:");
+            for (Estudante estudante : aprovados) {
+                System.out.println(estudante.getNome() + " - Média: " + estudante.calculaMedia());
+            }
+        }
     }
 }
