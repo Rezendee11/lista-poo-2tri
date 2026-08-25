0b785d4caf4f01e835b1978027c88f757f5ffafd
Rezendee11 <pedrorsantanna@gmail.com>
2026-08-25 11:30:04 -0300

    Commit 3: Exercício 2 completo

---
+++ src/Estudante.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Estudante {
+    private String nome;
+    private double[] notas;
+
+    public Estudante(String nome) {
+        this.nome = nome;
+        this.notas = new double[5];
+    }
+
+    public void insereNotas() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Nota " + (i + 1) + " de " + nome + ": ");
+            notas[i] = scanner.nextDouble();
+        }
+    }
+
+    public double calculaMedia() {
+        double soma = 0;
+        for (double nota : notas) {
+            soma += nota;
+        }
+        return soma / notas.length;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public double menorNota() {
+        double menor = notas[0];
+        for (double nota : notas) {
+            if (nota < menor) {
+                menor = nota;
+            }
+        }
+        return menor;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nome do estudante: ");
+        String nome = scanner.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Estudante estudante = new Estudante(nome);
+        estudante.insereNotas();
+
+        System.out.println("Média: " + estudante.calculaMedia());
+        System.out.println("Menor nota: " + estudante.menorNota());
     }
 }
